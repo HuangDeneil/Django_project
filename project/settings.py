aa69aa2f3b5e59a29c53f67ab4e36ac1b500bcd0
hudeneil <h7106019005@smail.nchu.edu.tw>
2021-02-24 01:15:59 +0000

    ip setting to *

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i_oxyseih=y2x*)f@knum03)&v=7sf5pvpdk!_s5ur#nztz^t6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.2.96','192.168.3.224','192.168.3.189','192.168.3.175','192.168.3.41']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
